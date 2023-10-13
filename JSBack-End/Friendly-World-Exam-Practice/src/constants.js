7e8e41c42c24f44d9ea7c034a0eaa7b2647dfc72
ProstoKris <kristianVal4ev@gmail.com>
2023-10-13 16:37:58 +0300

    Update constants and make user service

--- JSBack-End/Friendly-World-Exam-Practice/src/constants.js
+++ JSBack-End/Friendly-World-Exam-Practice/src/constants.js
@@ -1,6 +1,6 @@
 const constants = {
   PORT: '3000',
-  DB_CONNECTION_STRING: 'mongodb://127.0.0.1:27017',
+  DB_CONNECTION_STRING: 'mongodb://127.0.0.1:27017/friendly-world',
 };
 
 module.exports = constants;
