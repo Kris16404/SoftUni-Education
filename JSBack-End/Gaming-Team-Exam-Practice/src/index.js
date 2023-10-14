7e54fdb83b230f2d86f282c0c251c1cda2f88fb1
ProstoKris <kristianVal4ev@gmail.com>
2023-10-14 16:54:35 +0300

    Use config files on express app

--- JSBack-End/Gaming-Team-Exam-Practice/src/index.js
+++ JSBack-End/Gaming-Team-Exam-Practice/src/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 
 const { PORT } = require('./utils/constants.js');
+const { expressConfig } = require('./config/expressConfig.js');
+const { handlebarsConfig } = require('./config/handlebarsConfig.js');
 
 const app = express();
+
+expressConfig(app);
+handlebarsConfig(app);
 
 app.listen(PORT, () => console.log(`The app is listening on port : ${PORT}`));
