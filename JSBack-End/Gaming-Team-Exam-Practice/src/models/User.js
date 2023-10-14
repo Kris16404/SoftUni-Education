7533f5ceb3c0dfa0941f9e678c8a74617e3071dc
ProstoKris <kristianVal4ev@gmail.com>
2023-10-14 17:54:38 +0300

    Add register logic for DB

--- JSBack-End/Gaming-Team-Exam-Practice/src/models/User.js
+++ JSBack-End/Gaming-Team-Exam-Practice/src/models/User.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+  username: { type: String, required: true, min: 5 },
+  email: { type: String, required: true, min: 10 },
+  password: { type: String, required: true, min: 4 },
 });
 
 const User = mongoose.model('User', userSchema);
--- JSBack-End/Gaming-Team-Exam-Practice/src/services/userService.js
+++ JSBack-End/Gaming-Team-Exam-Practice/src/services/userService.js
@@ -1,9 +1,30 @@
 const User = require('../models/User.js');
+const bcrypt = require('bcrypt');
 
 exports.login = (email, password) => {
   //
 };
 
-exports.register = (email, password) => {
-  //
+exports.register = async (username, email, password, rePass) => {
+  if (password !== rePass) {
+    throw new Error(`Passwords don't match`);
+  }
+
+  const checkForExistingEmail = User.findOne({ email: email });
+
+  if (!checkForExistingEmail) {
+    throw new Error('And user with this email already exists');
+  }
+
+  const hash = await bcrypt.hash(password, 10);
+
+  const userObj = {
+    username: username,
+    email: email,
+    password: hash,
+  };
+
+  const user = new User(userObj);
+
+  user.save();
 };
