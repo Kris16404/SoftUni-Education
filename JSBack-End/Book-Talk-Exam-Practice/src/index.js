865867a884c590fb06710db2065e0482270b16ce
ProstoKris <kristianVal4ev@gmail.com>
2023-10-16 15:39:43 +0300

    Install express-handlebars and configure the app

--- JSBack-End/Book-Talk-Exam-Practice/src/index.js
+++ JSBack-End/Book-Talk-Exam-Practice/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const { expressConfig } = require('./config/expressConfig.js');
+const { handlebarsConfig } = require('./config/handlebarsConfig.js');
 const { PORT } = require('./utils/constants.js');
 
 const app = express();
 
+handlebarsConfig(app);
 expressConfig(app);
 
 app.listen(PORT, () => console.log(`The Server Is Listening On Port: ${PORT}`));
