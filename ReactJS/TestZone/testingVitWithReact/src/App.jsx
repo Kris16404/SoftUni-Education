455373a5aac4e4097767ad9f9019cc56e9b81649
ProstoKris <kristianVal4ev@gmail.com>
2023-10-26 16:21:53 +0300

    Starting to learn React

---
+++ ReactJS/TestZone/testingVitWithReact/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import TestComponent from './components/TestComponent.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, first timer here</h1>
+      <TestComponent />
+    </div>
+  );
+}
+
+export default App;
