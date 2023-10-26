032917ffafda642fc031b2048e249a3c7913f155
ProstoKris <kristianVal4ev@gmail.com>
2023-10-26 16:28:11 +0300

    Experiment with useState function

--- ReactJS/TestZone/testingVitWithReact/src/App.jsx
+++ ReactJS/TestZone/testingVitWithReact/src/App.jsx
@@ -3,10 +3,17 @@ import TestComponent from './components/TestComponent.jsx';
 import './App.css';
 
 function App() {
+  const [click, setClick] = useState(0);
+
+  function addClick() {
+    setClick(click + 1);
+  }
+
   return (
     <div>
       <h1>Hello, first timer here</h1>
       <TestComponent />
+      <button onClick={addClick}>Clicked {click} Times</button>
     </div>
   );
 }
