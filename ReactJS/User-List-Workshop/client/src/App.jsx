04a6f6108664a717707eb6b8fed2762396d7685e
ProstoKris <kristianVal4ev@gmail.com>
2023-11-11 17:00:50 +0200

    Create  componentsand configure index

--- ReactJS/User-List-Workshop/client/src/App.jsx
+++ ReactJS/User-List-Workshop/client/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles.css';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import SearchBar from './components/SearchBar.jsx';
+import Table from './components/Table.jsx';
+import Pagination from './components/Pagination.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <main className="main">
+        <section className="card users-container">
+          <SearchBar />
+          <Table />
+
+          {/* New user button   */}
+          <button className="btn-add btn">Add new user</button>
+
+          <Pagination />
+        </section>
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
