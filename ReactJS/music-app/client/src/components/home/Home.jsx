94230bdcb3e49adce95a3376314e7651b3ab233a
ProstoKris <kristianVal4ev@gmail.com>
2023-11-25 17:57:39 +0200

    Make simple welcome page

--- ReactJS/music-app/client/src/components/home/Home.jsx
+++ ReactJS/music-app/client/src/components/home/Home.jsx
@@ -1,5 +1,17 @@
+import 'home.css';
+
 const Home = () => {
-  return <h1>This is home page</h1>;
+  <div className="container">
+    <h1 className="title">Welcome to Your Music Collection</h1>
+    <p className="subtitle">
+      Explore, manage, and enjoy your favorite music all in one place.
+    </p>
+    <img
+      src="https://via.placeholder.com/400" // Replace with your own music-related image
+      alt="Music Notes"
+      className="image"
+    />
+  </div>;
 };
 
 export default Home;
