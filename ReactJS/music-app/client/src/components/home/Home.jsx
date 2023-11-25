3e990f025ea37b087f7ad40027dcb78810b0a0eb
ProstoKris <kristianVal4ev@gmail.com>
2023-11-25 18:05:19 +0200

    Fix home page

--- ReactJS/music-app/client/src/components/home/Home.jsx
+++ ReactJS/music-app/client/src/components/home/Home.jsx
@@ -1,17 +1,25 @@
-import 'home.css';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+
+import './home.css';
 
 const Home = () => {
-  <div className="container">
-    <h1 className="title">Welcome to Your Music Collection</h1>
-    <p className="subtitle">
-      Explore, manage, and enjoy your favorite music all in one place.
-    </p>
-    <img
-      src="https://via.placeholder.com/400" // Replace with your own music-related image
-      alt="Music Notes"
-      className="image"
-    />
-  </div>;
+  return (
+    <Container className="home-container">
+      <Row>
+        <Col md={{ span: 8, offset: 2 }}>
+          <div className="jumbotron">
+            <h1>Welcome to Your Music Collection</h1>
+            <p>
+              Explore, manage, and enjoy your favorite music all in one place.
+            </p>
+            <p>
+              <Button variant="primary">Get Started</Button>
+            </p>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Home;
