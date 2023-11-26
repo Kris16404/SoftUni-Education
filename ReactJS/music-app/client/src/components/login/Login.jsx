b6b094468c13f20f0c3c6dfcdd614eb704c253da
ProstoKris <kristianVal4ev@gmail.com>
2023-11-26 17:19:00 +0200

    Add login view

--- ReactJS/music-app/client/src/components/login/Login.jsx
+++ ReactJS/music-app/client/src/components/login/Login.jsx
@@ -1,5 +1,47 @@
+import React, { useState } from 'react';
+import { Container, Form, Button, FloatingLabel } from 'react-bootstrap';
+import './login.css'; // Import the CSS file for custom styling
+
 const Login = () => {
-  return <h1>This is the Login page</h1>;
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your login logic here
+    console.log('Login form submitted:', formData);
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h2>Login</h2>
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Email address"
+          className="mb-3"
+        >
+          <Form.Control type="email" placeholder="name@example.com" />
+        </FloatingLabel>
+        <FloatingLabel controlId="floatingPassword" label="Password">
+          <Form.Control type="password" placeholder="Password" />
+        </FloatingLabel>
+        <Button variant="primary" type="submit" className="mt-3">
+          Login
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
