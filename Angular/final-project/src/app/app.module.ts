697f8078b99a9339ce90deaf69be4a9982fa265d
ProstoKris <kristianVal4ev@gmail.com>
2024-03-09 13:14:55 +0200

    Setup Firebase

--- Angular/final-project/src/app/app.module.ts
+++ Angular/final-project/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { CoreModule } from './core/core.module';
+import { environment } from 'src/environments/environment.development';
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    CoreModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
