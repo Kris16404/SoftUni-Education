a61cea17be64af522f49598acfb5ae53f92b245e
ProstoKris <kristianVal4ev@gmail.com>
2024-03-19 20:40:54 +0200

    Include HttpModule

--- Angular/final-project/src/app/app.module.ts
+++ Angular/final-project/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AngularFireModule } from '@angular/fire/compat';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
+    HttpClientModule,
     CoreModule,
     SharedModule,
   ],
