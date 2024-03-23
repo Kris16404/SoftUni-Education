e9bda9662169f943df0535430403fc064f1c94b8
ProstoKris <kristianVal4ev@gmail.com>
2024-03-23 12:02:06 +0200

    Make login page

--- Angular/final-project/src/app/shared/login-page/login-page.component.ts
+++ Angular/final-project/src/app/shared/login-page/login-page.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.css']
+  styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent {
+  constructor(private fb: FormBuilder) {}
 
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
+
+  login(): void {
+    if (this.form.invalid) {
+      return;
+    }
+
+    console.log(this.form.value);
+  }
 }
