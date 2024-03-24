5b7fb7b7596b76189f7f4ca29ac831690ff92651
ProstoKris <kristianVal4ev@gmail.com>
2024-03-24 10:43:34 +0200

    Add logout functionality

--- Angular/final-project/src/app/core/nav-bar/nav-bar.component.ts
+++ Angular/final-project/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
 export class NavBarComponent {
+  constructor(private userService: UserService) {}
 
+  logout() {
+    this.userService
+      .signOut()
+      .then(() => {
+        sessionStorage.clear();
+      })
+      .catch((err) => console.error(err));
+  }
 }
