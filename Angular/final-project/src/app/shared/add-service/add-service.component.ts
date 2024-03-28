8cf11038a3ff47b99920fe6a6cf9cdae8bfd654d
ProstoKris <kristianVal4ev@gmail.com>
2024-03-28 16:44:26 +0200

    Start doing add service

--- Angular/final-project/src/app/shared/add-service/add-service.component.ts
+++ Angular/final-project/src/app/shared/add-service/add-service.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-add-service',
   templateUrl: './add-service.component.html',
-  styleUrls: ['./add-service.component.css']
+  styleUrls: ['./add-service.component.css'],
 })
 export class AddServiceComponent {
+  constructor(private fb: FormBuilder) {}
 
+  form = this.fb.group({
+    name: [''],
+    imageUrl: [''],
+    price: [0],
+    description: [''],
+  });
 }
