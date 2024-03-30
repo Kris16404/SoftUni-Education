7e6375449b8df048a69a0605a67c5f535288824c
ProstoKris <kristianVal4ev@gmail.com>
2024-03-30 17:05:08 +0200

    Add a  profile page that shows your services

--- Angular/final-project/src/app/shared/profile-page/profile-page.component.ts
+++ Angular/final-project/src/app/shared/profile-page/profile-page.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/services/post.service';
+import { Service } from 'src/app/types/Service';
 
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
-  styleUrls: ['./profile-page.component.css']
+  styleUrls: ['./profile-page.component.css'],
 })
-export class ProfilePageComponent {
+export class ProfilePageComponent implements OnInit {
+  services: Service[] = [];
+  user = JSON.parse(sessionStorage.getItem('user')!);
+  constructor(private postService: PostService) {}
 
+  ngOnInit(): void {
+    this.postService.getCommunityServices().subscribe((services) => {
+      this.writeFilterdServices(services);
+    });
+  }
+  writeFilterdServices(services: any) {
+    Object.keys(services).forEach((key: any) => {
+      const temp: Service = { id: key, ...services[key] };
+      this.services.push(temp);
+    });
+
+    this.services = this.services.filter(
+      (service) => service.ownerId === this.user.id
+    );
+  }
 }
