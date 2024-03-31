64da2c74774dfa1520f2fff2dc219c14276e3435
ProstoKris <kristianVal4ev@gmail.com>
2024-03-31 13:27:28 +0300

    Make validation for front end

--- Angular/final-project/src/app/shared/add-service/add-service.component.ts
+++ Angular/final-project/src/app/shared/add-service/add-service.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
-import { urlCheck } from '../utils/url-check-validator';
 import { ServiceForPostReq } from 'src/app/types/Service';
 import { PostService } from 'src/app/services/post.service';
 import { Router } from '@angular/router';
@@ -17,19 +16,25 @@ export class AddServiceComponent {
     private router: Router
   ) {}
 
-  form = this.fb.group(
-    {
-      name: ['', [Validators.required, Validators.minLength(5)]],
-      imageUrl: ['', [Validators.required]],
-      price: ['', [Validators.required]],
-      description: ['', [Validators.required, Validators.minLength(10)]],
-      termsCheckbox: [false, Validators.requiredTrue],
-    },
-    {
-      validators: [urlCheck('imageUrl')],
-    }
-  );
+  form = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(5)]],
+    imageUrl: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(
+          '^data:image/(jpeg|png|gif|bmp|webp);base64,([a-zA-Z0-9+/]+={0,2})(;base64)?'
+        ),
+      ],
+    ],
+    price: ['', [Validators.required, Validators.min(1)]],
+    description: ['', [Validators.required, Validators.minLength(10)]],
+    termsCheckbox: [false, Validators.requiredTrue],
+  });
 
+  log() {
+    console.log(this.form.get('imageUrl')?.errors);
+  }
   postSubmit() {
     if (this.form.invalid) {
       return;
